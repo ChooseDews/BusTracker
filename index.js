dff324027731a71fffed9733834c4832d24ecf38
Ubuntu <john@BusTracker.4qz2iawehd0e3o0q3uyigf1zxd.bx.internal.cloudapp.net>
2018-10-18 19:53:02 +0000

    Added stop information

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const lodash = require('lodash');
 
 var route_info_url = "http://feeds.transloc.com/3/routes?agencies=116";
 var url = "http://feeds.transloc.com/3/vehicle_statuses?agencies=116";
-
+var routes_stops_url = "http://feeds.transloc.com/3/stops?include_routes=true&agencies=116";
 
 var ohGod = function(err) {
   console.log('We Had An Error :(  ');
@@ -20,7 +20,40 @@ var gather = function(url) {
   }).catch(ohGod);
 }
 
-//gather(url).then(console.log)
+
+var stopInformation = null;
+var routeStops = null;
+
+var getRouteStops = async function(id) {
+  if (!routeStops) {
+    var a = (await gather(routes_stops_url));
+    routeStops = a.routes;
+    stopInformation = a.stops;
+  }
+  var s = lodash.find(routeStops, {id});
+
+   var stops = [];
+  
+  for(var index in s.stops){
+      var n = s.stops[index];
+      stops.push(await getStopInfo(n));
+   }
+   //console.log(stops.length)
+
+ 
+  return stops;
+}
+
+
+var getStopInfo = async function(stop_id) {
+  if (!stopInformation) {
+    var a = (await gather(routes_stops_url));
+    routeStops = a.routes;
+    stopInformation = a.stops;
+  }
+  return lodash.find(stopInformation, {id: stop_id})
+}
+
 
 var routeInformation = null;
 
@@ -33,6 +66,7 @@ var getRouteInfo = async function(id) {
   var a = lodash.find(routeInformation, {
     id: id
   });
+  delete a.url;
   return a;
 };
 
@@ -41,8 +75,14 @@ var getBusesStatus = async function() {
   for (var index in busData) {
     var bus = busData[index];
     var route = await getRouteInfo(bus.route_id);
+    var stops = await getRouteStops(bus.route_id);
     busData[index].route = route;
+    busData[index].stops = stops;
+    // console.log(route)
+    delete busData[index].apc_status;
+    delete busData[index].num_cars;
   }
+  console.log(busData);
   return busData;
 };
 
@@ -57,9 +97,9 @@ const port = 3000;
 app.get('/', async function(req, res) {
   var busData = await getBusesStatus();
 
-  console.log(busData);
+  //console.log(busData);
   busData = lodash.groupBy(busData, 'route.long_name');
-  console.log(busData);
+  //console.log(busData);
 
   res.render('buses', {
     routes: busData
